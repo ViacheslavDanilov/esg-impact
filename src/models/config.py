4f6d2e2936cf55f410a624c7dbe60ee952fac163
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2025-03-30 21:21:13 +0200

    [Feature]: add configuration settings for model training

---
+++ src/models/config.py
@@ -0,0 +1,22 @@
+from typing import List
+
+from pydantic import BaseModel
+
+
+class TrainingSettings(BaseModel):
+    """Configuration settings for model training."""
+
+    # TODO: add more parameters
+    db_host: str = "test"
+    db_port: int = 123
+    db_name: str = "test"
+    db_user: str = "test"
+
+
+class Settings(BaseModel):
+    """Main configuration class."""
+
+    train: TrainingSettings = TrainingSettings()
+
+
+cfg = Settings()
