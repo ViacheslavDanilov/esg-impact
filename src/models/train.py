3ff9ed0625e5c5fe537c909a3b1881baa0fb9a90
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2025-04-06 21:08:43 +0200

    [Feature]: integrate DataScaler for feature normalization in training process

--- src/models/train.py
+++ src/models/train.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 import pandas as pd
 import shap
@@ -7,6 +8,8 @@ from econml.dml import LinearDML
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 
+from src import ROOT_DIR
+from src.data.scaler import DataScaler
 from src.data.utils import generate_mock_data
 
 logging.basicConfig(
@@ -21,31 +24,26 @@ logger = logging.getLogger(__name__)
 
 
 def main():
-    # Загрузка исходных данных
+    # Load the raw data
     df_raw = pd.read_csv(cfg.train.data_path)
 
-    # Генерация синтетических данных
+    # Generate synthetic data
     df = generate_mock_data(df_raw, num_rows=500, seed=11)
 
-    # Зависимая переменная (Y_params) = TOBIN_Q_RATIO
+    # Target variable (Y_params) = TOBIN_Q_RATIO
     Y = df[cfg.train.Y_params]
 
-    # Вмешательство (T_params) = ESG-показатели
+    # Treatment variable (T_params) = ESG scores
     T = df[cfg.train.T_params]
 
-    # Контрольные переменные (W_params) = макро + фундаментальные
+    # Control variables (W_params) = macro + fundamental
     W = df[cfg.train.W_params]
 
-    # Переменные для гетерогенности (X_params) - Признаки, от которых зависит сила эффекта ESG
-    # Если мы хотим оценить, как влияние ESG на Q Tobin варьируется в зависимости от:
-    # Размера компании (CRNCY_ADJ_MKT_CAP),
-    # Сектора (FINANCIAL_DUMMY),
-    # Года наблюдения (Year).
-    # Если гетерогенность не требуется, X_params=None или константа.
+    # Variables for heterogeneity (X_params) = features that affect the strength of the ESG effect
     # TODO: define X_params in order to use the model
     X = df[cfg.train.X_params] if cfg.train.X_params else pd.DataFrame(index=df.index)
 
-    # Разделение данных на обучающую и валидационную выборки
+    # Split the data into training and validation sets
     X_train, X_test, Y_train, Y_test, T_train, T_test, W_train, W_test = train_test_split(
         X,
         Y,
@@ -56,32 +54,45 @@ def main():
         shuffle=True,
     )
 
-    # Инициализация модели LinearDML с RandomForestRegressor для model_y и model_t
+    # TODO: think which columns to normalize
+    # Initialize the DataScaler
+    scaler_path_w = os.path.join(ROOT_DIR, "data", "scaler_w.pkl")
+    scaler_w = DataScaler(scaler_path_w)
+    scaler_path_x = os.path.join(ROOT_DIR, "data", "scaler_x.pkl")
+    scaler_x = DataScaler(scaler_path_x)
+
+    # Fit and transform the training data
+    W_train_scaled = scaler_w.fit_transform(W_train)
+    W_test_scaled = scaler_w.transform(W_test)
+    X_train_scaled = scaler_x.fit_transform(X_train)
+    X_test_scaled = scaler_x.transform(X_test)
+
+    # Inittialize the LinearDML model with RandomForestRegressor for model_y and model_t
     model = LinearDML(
         model_y=RandomForestRegressor(n_estimators=100, random_state=cfg.train.seed),
         model_t=RandomForestRegressor(n_estimators=100, random_state=cfg.train.seed),
         random_state=cfg.train.seed,
     )
 
-    # Обучение модели на обучающей выборке
+    # Train the model on the training set
     model.fit(
         Y=Y_train,
         T=T_train,
-        X=X_train,  # TODO: define X_params in order to use the model
-        W=W_train,
+        X=X_train_scaled,  # TODO: define X_params in order to use the model
+        W=W_train_scaled,
     )
 
-    ate = model.ate(X)  # Средний эффект ESG
-    cate = model.effect(X)  # Гетерогенные эффекты
+    ate = model.ate(X_test_scaled)  # Average Treatment Effect (ATE)
+    cate = model.effect(X_test_scaled)  # Heterogeneous Treatment Effect (CATE)
     logger.info(f"Average Treatment Effect (ATE): {ate}")
     logger.info(f"Conditional Average Treatment Effect (CATE): {cate}")
 
-    # Получение SHAP-значений для модели
+    # Compute SHAP values for the model
     explainer = shap.Explainer(model.model_final)
-    shap_values = explainer(X_test)
+    shap_values = explainer(X_test_scaled)
 
-    # Визуализация SHAP-значений
-    shap.summary_plot(shap_values, X_test)
+    # Visualize SHAP values
+    shap.summary_plot(shap_values, X_test_scaled)
 
     logger.info("Complete")
 
